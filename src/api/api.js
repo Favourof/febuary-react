3e40a423aa3b5d5936e7c086c12e8ee07c50d1d9
ezekiel <favourtobiloba200@gmail.com>
2026-03-30 12:05:00 +0100

    env setup

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:4001";
+const baseURL = import.meta.env.VITE_API_URL;
 export const publicIntance = axios.create({ baseURL });
 
 export const privateInstance = axios.create({ baseURL });
